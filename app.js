2beea027bbe9ee537dd8501b6445d4eeb3f56167
johnjosephomolon <johnjosephomolon@gmail.com>
2022-11-18 12:37:26 +0800

    not final

--- app.js
+++ app.js
@@ -49,3 +49,28 @@ function opentab(tabname) {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+var sidemenu = document.getElementById('sidemenu');
+
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-200px";
+}
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbySzMT_SBUeQmKcuNbaihESwMO09VHo3j4PVaJdpDACHK5o__IYmA7bUpvmPu-Li7XT/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+var myText = "Message sent successfully"
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = alert(myText)
+            form.reset();
+        })
+        .catch(error => console.error('Error!', error.message))
+})
